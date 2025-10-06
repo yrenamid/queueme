f390b7de00dd99620a79c9cbc91f616798dd5d71
yrenamid <tahamidclyren1@gmail.com>
2025-10-06 22:05:46 +0800

    Add JSON and URL-encoded body parsing middleware

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,8 @@ app.use((req,res,next) => {
 	next();
 });
 
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 app.use(bodyParser.json({ limit: '1mb' }));
 app.use(morgan('dev'));
 
